98d80dd4d527d8764ceb29994a241be68df4a835
Khachornchit Songsaen <khachornchit@chorn.in.th>
2024-09-02 17:04:26 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // For Static Export
-  // output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  optimizeFonts: false,
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
+    optimizeFonts: false,
 };
 
 export default nextConfig;
