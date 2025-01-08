d9a1e51c0bc518473af97963844213d9c475a1ac
Khachornchit Songsaen <khachornchit@chorn.in.th>
2025-01-08 17:46:50 +0700

    Improve banner image.

--- src/components/AI/MainBannerAi/right.tsx
+++ src/components/AI/MainBannerAi/right.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import {Info} from "@/data/info/Info";
+import Link from "next/link";
 
 export default function Right() {
 
     return (
         <div className="col-lg-6">
-            <Image
-                src={Info.Images.home.mainBanner.path}
-                alt="image"
-                width={1200}
-                height={630}
-            />
+            <div className="main-banner-image main-banner-image-custom">
+                <Image
+                    src={Info.Images.home.mainBanner.path}
+                    className={'image-opacity'}
+                    alt="image"
+                    width={1200}
+                    height={630}
+                />
+            </div>
         </div>
     )
 }
