024c1c9405d3e7a112ad040cbd2c35777d8df3bb
Khachornchit Songsaen <khachornchit@chorn.in.th>
2025-01-29 15:28:54 +0700

    Update photos.

--- src/components/AI/HomeAiSolutionBanner/left.tsx
+++ src/components/AI/HomeAiSolutionBanner/left.tsx
@@ -7,15 +7,6 @@ export default function Left() {
     return (
         <div className="col-lg-6">
             <div className="main-banner-content pr-15">
-                {/*<h1*/}
-                {/*    data-aos="fade-up"*/}
-                {/*    data-aos-duration="800"*/}
-                {/*    data-aos-delay="100"*/}
-                {/*    data-aos-once="true"*/}
-                {/*    className="mobile-optimized-heading"*/}
-                {/*>*/}
-                {/*    {Info.Service.title}*/}
-                {/*</h1>*/}
 
                 <p
                     data-aos="fade-up"
@@ -25,17 +16,6 @@ export default function Left() {
                     className={'mb-4'}
                     dangerouslySetInnerHTML={{__html: Info.Service.description} as { __html: string }}
                 />
-
-                <div className={'mb-4'}>
-                    <a href={Info.Contact.chorn.link} target={'_blank'}>
-                        <Image
-                            src={Info.Contact.chorn.iconImageLink}
-                            alt="เพิ่มเพื่อน"
-                            width={120}
-                            height={36}
-                        />
-                    </a>
-                </div>
 
             </div>
         </div>
