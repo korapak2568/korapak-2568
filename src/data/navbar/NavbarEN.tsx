50e38603c14a1041536c578bfa89464c34e49998
Khachornchit Songsaen <khachornchit@chorn.in.th>
2025-04-06 23:20:14 +0700

    Update LINE-AI Integration.

--- src/data/navbar/NavbarEN.tsx
+++ src/data/navbar/NavbarEN.tsx
@@ -39,13 +39,13 @@ export const NavbarEN: INavbar[] = [
             },
             {
                 group: "/technical-expertise/",
-                label: "LLM-AI LINE Chat Friend",
+                label: "LLM-AI LINE Integration",
                 link: "/ai-companions/fah/",
                 submenu: [],
             },
             {
                 group: "/technical-expertise/",
-                label: "LLM-AI Consultant",
+                label: "LLM-AI Integration",
                 link: "/technical-expertise/ai-solutions/",
                 submenu: [],
             },
