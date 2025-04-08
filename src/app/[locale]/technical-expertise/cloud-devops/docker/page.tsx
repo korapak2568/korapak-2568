f9f7271869b77fe5a2d3e2f08776edcdcf5c06a9
Khachornchit Songsaen <khachornchit@chorn.in.th>
2025-04-09 00:41:55 +0700

    It did not deploy at the Vercel then tried to remove the importing of some PageBanner to re-deploy.

--- src/app/[locale]/technical-expertise/cloud-devops/docker/page.tsx
+++ src/app/[locale]/technical-expertise/cloud-devops/docker/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "@/components/Layouts/Navbar";
-import PageBanner from "@/components/Common/PageBanner";
 import Footer from "@/components/Layouts/Footer";
 import CloudExperience from "@/components/Common/CloudExperience";
 import ServicesDetailsDocker from "@/components/Services/cloud-devops/docker/ServicesDetailsDocker";
@@ -8,7 +7,6 @@ import {Metadata} from "next";
 import {SchemaMarkupServicePage} from "@/components/GoogleSchemaMarkup/SchemaMarkupServicePage";
 import {headers} from "next/headers";
 import {MetadataDocker} from "@/metadata/pages/devops/docker/common/MetadataDocker";
-import {InfoTranslation} from "@/data/info/main/InfoTranslation";
 
 export async function generateMetadata(): Promise<Metadata> {
     const lang = headers().get('x-locale') || 'en';
--- src/app/[locale]/technical-expertise/full-stack-developer/go-developer/page.tsx
+++ src/app/[locale]/technical-expertise/full-stack-developer/go-developer/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "@/components/Layouts/Navbar";
-import PageBanner from "@/components/Common/PageBanner";
 import Footer from "@/components/Layouts/Footer";
 import ServicesDetailsGoLang from "@/components/Services/fullstack-development/go-lang/ServicesDetailsGoLang";
 import CloudExperience from "@/components/Common/CloudExperience";
