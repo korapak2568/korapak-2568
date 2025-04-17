1af3e195b12a05814666757c73104c228717aeb1
Khachornchit Songsaen <khachornchit@chorn.in.th>
2025-04-17 20:18:19 +0700

    Change the metadata path back to /internal-images/

--- src/metadata/opengraph/OpenGraphDevOps.ts
+++ src/metadata/opengraph/OpenGraphDevOps.ts
@@ -3,43 +3,43 @@ import {TrackVersion} from "../version/MetadataVersion";
 
 export const OpenGraphDevOps: IOpenGraphDevOps = {
     main: {
-        url: "https://chorn.in.th/images/metadata/devops/devops-services.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/devops-services.png" + TrackVersion,
         alt: "CHORN's image of DevOps & Testing Services Banner"
     },
     appium: {
-        url: "https://chorn.in.th/images/metadata/devops/appium.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/appium.png" + TrackVersion,
         alt: "CHORN's image of Appium Testing Banner"
     },
     docker: {
-        url: "https://chorn.in.th/images/metadata/devops/docker.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/docker.png" + TrackVersion,
         alt: "CHORN's image of Docker DevOps Banner"
     },
     github: {
-        url: "https://chorn.in.th/images/metadata/devops/github.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/github.png" + TrackVersion,
         alt: "CHORN's image of GitHub DevOps Banner"
     },
     gitlab: {
-        url: "https://chorn.in.th/images/metadata/devops/gitlab.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/gitlab.png" + TrackVersion,
         alt: "CHORN's image of GitLab DevOps Banner"
     },
     jenkins: {
-        url: "https://chorn.in.th/images/metadata/devops/jenkins.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/jenkins.png" + TrackVersion,
         alt: "CHORN's image of Jenkins DevOps Banner"
     },
     kube: {
-        url: "https://chorn.in.th/images/metadata/devops/kubernetes.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/kubernetes.png" + TrackVersion,
         alt: "CHORN's image of Kubernetes DevOps Banner"
     },
     postman: {
-        url: "https://chorn.in.th/images/metadata/devops/postman.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/postman.png" + TrackVersion,
         alt: "CHORN's image of Postman API Testing Banner"
     },
     selenium: {
-        url: "https://chorn.in.th/images/metadata/devops/selenium.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/selenium.png" + TrackVersion,
         alt: "CHORN's image of Selenium Testing Banner"
     },
     soapui: {
-        url: "https://chorn.in.th/images/metadata/devops/soapui.png" + TrackVersion,
+        url: "https://chorn.in.th/internal-images/devops/soapui.png" + TrackVersion,
         alt: "CHORN's image of SoapUI Testing Banner"
     },
 }
