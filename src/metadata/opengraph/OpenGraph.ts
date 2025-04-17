102503f131c23359ac409c068358a2498cf6a554
Khachornchit Songsaen <khachornchit@chorn.in.th>
2025-04-17 20:05:56 +0700

    Change metadata image path to /images/metadata

--- src/metadata/opengraph/OpenGraph.ts
+++ src/metadata/opengraph/OpenGraph.ts
@@ -4,54 +4,57 @@ import {OpenGraphBackEnd} from "@/metadata/opengraph/OpenGraphBackEnd";
 import {OpenGraphDevOps} from "@/metadata/opengraph/OpenGraphDevOps";
 import {TrackVersion} from "../version/MetadataVersion";
 
+// Using Vercel - /internal-images/
+// Use CloudFlare - /images/metadata/
+
 export const OpenGraph: IOpenGraph = {
     home: {
-        url: "https://chorn.in.th/internal-images/home/home-og-image.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/home/home-og-image.jpg" + TrackVersion,
         alt: "CHORN's image of home flexible workplace"
     },
     llmAi: {
-        url: "https://chorn.in.th/internal-images/banner/chorn-banner.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/banner/chorn-banner.jpg" + TrackVersion,
         alt: "CHORN's image of LLM/AI integration",
     },
     aiCompanions: {
-        url: "https://chorn.in.th/internal-images/ai/ai-companions-og-image.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/ai/ai-companions-og-image.jpg" + TrackVersion,
         alt: "CHORN's image of AI companions",
     },
     aiFah: {
-        url: "https://chorn.in.th/internal-images/ai/ai-og-fah.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/ai/ai-og-fah.jpg" + TrackVersion,
         alt: "CHORN's image of AI-Fah - LINE Chat AI Integration",
     },
     aiAom: {
-        url: "https://chorn.in.th/internal-images/ai/ai-og-aom.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/ai/ai-og-aom.jpg" + TrackVersion,
         alt: "CHORN's image of AI-Aom - LINE Chat AI Integration",
     },
     aiPloy: {
-        url: "https://chorn.in.th/internal-images/ai/ai-og-ploy.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/ai/ai-og-ploy.jpg" + TrackVersion,
         alt: "CHORN's image of AI-Ploy - LINE Chat AI Integration",
     },
     gallery: {
-        url: "https://chorn.in.th/internal-images/gallery/gallery-og-image.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/gallery/gallery-og-image.jpg" + TrackVersion,
         alt: "CHORN's image of gallery",
     },
     about: {
-        url: "https://chorn.in.th/internal-images/about/about-og-image.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/about/about-og-image.jpg" + TrackVersion,
         alt: "CHORN's image of about"
     },
     contact: {
-        url: "https://chorn.in.th/internal-images/contact/contact-og-image.jpg" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/contact/contact-og-image.jpg" + TrackVersion,
         alt: "CHORN's image of contacting"
     },
 
     cloudSolutions: {
-        url: "https://chorn.in.th/internal-images/banner/banner-logo.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/banner/banner-logo.png" + TrackVersion,
         alt: "CHORN's image of cloud solutions"
     },
     webDevelopment: {
-        url: "https://chorn.in.th/internal-images/frontend/frontend-development-services.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/frontend-development-services.png" + TrackVersion,
         alt: "CHORN's image of web development"
     },
     blockchain: {
-        url: "https://chorn.in.th/internal-images/banner/banner-logo.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/banner/banner-logo.png" + TrackVersion,
         alt: "CHORN's banner of web3 and blockchain development"
     },
 
--- src/metadata/opengraph/OpenGraphFrontEnd.ts
+++ src/metadata/opengraph/OpenGraphFrontEnd.ts
@@ -3,39 +3,39 @@ import {TrackVersion} from "../version/MetadataVersion";
 
 export const OpenGraphFrontEnd: IOpenGraphFrontEnd = {
     main: {
-        url: "https://chorn.in.th/internal-images/frontend/frontend-development-services.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/frontend-development-services.png" + TrackVersion,
         alt: "CHORN's image of Front-End Development Banner"
     },
     angular: {
-        url: "https://chorn.in.th/internal-images/frontend/angular-development-service.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/angular-development-service.png" + TrackVersion,
         alt: "CHORN's image of Angular Development Banner"
     },
     css3: {
-        url: "https://chorn.in.th/internal-images/frontend/css3-development-service.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/css3-development-service.png" + TrackVersion,
         alt: "CHORN's image of CSS3 Development Banner"
     },
     html5: {
-        url: "https://chorn.in.th/internal-images/frontend/html5-development-service.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/html5-development-service.png" + TrackVersion,
         alt: "CHORN's image of HTML5 Development Banner"
     },
     javascript: {
-        url: "https://chorn.in.th/internal-images/frontend/javascript-development-service.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/javascript-development-service.png" + TrackVersion,
         alt: "CHORN's image of CHORN JavaScript Development Banner"
     },
     nextjs: {
-        url: "https://chorn.in.th/internal-images/frontend/nextjs-development-service.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/nextjs-development-service.png" + TrackVersion,
         alt: "CHORN's image of Next.js Development Banner"
     },
     react: {
-        url: "https://chorn.in.th/internal-images/frontend/react-development-service.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/react-development-service.png" + TrackVersion,
         alt: "CHORN's image of React Development Banner"
     },
     typescript: {
-        url: "https://chorn.in.th/internal-images/frontend/typescript-development-service.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/typescript-development-service.png" + TrackVersion,
         alt: "CHORN's image of CHORN TypeScript Development Banner"
     },
     vue: {
-        url: "https://chorn.in.th/internal-images/frontend/vue-development-service.png" + TrackVersion,
+        url: "https://chorn.in.th/images/metadata/frontend/vue-development-service.png" + TrackVersion,
         alt: "Vue.js Development Banner"
     }
 }
