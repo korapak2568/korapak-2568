3621126ec22fd19b702a33e51107af565f6833a3
Khachornchit Songsaen <khachornchit@chorn.in.th>
2025-05-23 22:44:59 +0700

    Update

--- src/components/About/AboutLeft.tsx
+++ src/components/About/AboutLeft.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import {Contact2025} from "@/data/contact/Contact2025";
+import {FaMailBulk} from "react-icons/fa";
 
 export default function AboutLeft() {
     return (
@@ -8,26 +9,36 @@ export default function AboutLeft() {
             <div className="about-image-warp about-image-custom"/>
             <div className="x-bar-animation"/>
 
-            <div className="single-footer-widget about-linkedin-widget">
-                <div className="social social-about">
-                    <div className="linkedin-symbol">
-                        <a href="https://www.linkedin.com/company/chorn/"
-                           className="linkedin"
-                           target="_blank"
-                           aria-label="LinkedIn"
-                        >
-                            <i className="bx bxl-linkedin hover-rotate"/>
-                        </a>
+            <div className="single-footer-widget about-bottom-image">
+                <div className="social left">
+                    <div className="left-row">
+                        <div className="linkedin-symbol">
+                            <a href="https://www.linkedin.com/company/chorn/"
+                               className="linkedin"
+                               target="_blank"
+                               aria-label="LinkedIn"
+                            >
+                                <i className="bx bxl-linkedin hover-rotate"/>
+                            </a>
+                        </div>
+                        <div className="linkedin-text">
+                            <a href="https://www.linkedin.com/company/chorn/"
+                               target="_blank"
+                            >
+                                linkedin.com/chorn
+                            </a>
+                        </div>
                     </div>
-                    <div className="linkedin-text">
-                        <a href="https://www.linkedin.com/company/chorn/"
-                           target="_blank"
-                        >
-                            linkedin.com/chorn
-                        </a>
+                    <div className="left-row">
+                        <div className="symbol">
+                            <i className="bx bx-mail-send"/>
+                        </div>
+                        <p>
+                            contact@chorn.in.th
+                        </p>
                     </div>
                 </div>
-                <div>
+                <div className="right">
                     <a href={Contact2025.line.link}
                        target={'_blank'}>
                         <Image
