9a924a37a4218d5b1faece11528c733c8c1297b2
Khachornchit Songsaen <khachornchit@chorn.in.th>
2025-06-11 11:06:54 +0700

    Update contact image

--- src/components/Contact/ContactLeft.tsx
+++ src/components/Contact/ContactLeft.tsx
@@ -2,12 +2,11 @@ import "@/styles/about.scss"
 import React from "react";
 import BottomImageLeft from "@/components/About/BottomImage/BottomImageLeft";
 import BottomImageRight from "@/components/About/BottomImage/BottomImageRight";
-import {InfoTranslation} from "@/data/info/main/InfoTranslation";
 
 export default function ContactLeft({lang}: { lang: string }) {
     return (
         <div className="col-lg-4">
-            <div className="about-image-warp about-image-custom"/>
+            <div className="about-image-warp contact-image"/>
             <div className="x-bar-animation"/>
 
             <div className="single-footer-widget about-bottom-image">
